8c31daeeb6ee80b9fb0ce8494f4f2aab5ba5e3b1
Cenbylin <827418637@qq.com>
2018-07-28 15:10:44 +0800

    successfully use gpu. But the remained bug need to be fixed.

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-import retro
-import torch
-from multiprocessing.managers import BaseManager
-# from multiprocessing import Process
-import torch.multiprocessing as mp
 import os
+os.environ["OMP_NUM_THREADS"] = "1"
+import torch.multiprocessing as mp
+from torch.multiprocessing import SimpleQueue, Queue
 from option import get_opt
-from model import Actor, Learner, TraceQueue
+from model import Actor, Learner, QManeger
 import resource
-
 rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
 resource.setrlimit(resource.RLIMIT_NOFILE, (6144, rlimit[1]))
 
 
-# env = retro.make(game='SonicAndKnuckles3-Genesis')
-# sample = env.action_space.sample()
-class RemoteManager(BaseManager): pass
-
-
-def get_manager():
-    RemoteManager.register('TraceQueue', TraceQueue)
-    m = RemoteManager()
-    m.start()
-    return m
-
-
 if __name__ == '__main__':
     opt = get_opt()
-    # mp.set_start_method('spawn')
     os.environ["CUDA_VISIBLE_DEVICES"] = str(opt.gpu)
+    try:
+        mp.set_start_method('forkserver', force=True)
+        print("forkserver init")
+    except RuntimeError:
+        pass
 
-    # 进程共享数据
-    m = get_manager()
-    queue = m.TraceQueue()
-    learner = Learner(opt, queue)
-    actors = [Actor(opt, queue, learner), Actor(opt, queue, learner), Actor(opt, queue, learner)]
     processes = []
+    # data communication
+    q_trace = Queue(maxsize=300)
+    q_batch = Queue(maxsize=3)
+    q_manager = QManeger(opt, q_trace, q_batch)
+    p = mp.Process(target=q_manager.listening)
+    p.start()
+    processes.append(p)
+
+    learner = Learner(opt, q_batch)  # inner shared network was used by actors.
+    actors = [Actor(opt, q_trace, learner), Actor(opt, q_trace, learner), Actor(opt, q_trace, learner)]
     for rank, a in enumerate(actors):
         p = mp.Process(target=a.performing, args=(rank,))
         p.start()
         processes.append(p)
+
     learner.learning()
     for p in processes:
         p.join()
